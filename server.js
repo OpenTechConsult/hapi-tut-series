e1560614c40a66172b83f3f1ac28fb4f22047650
Elom Atsou Agboka <opentech.consult@gmail.com>
2023-12-20 16:12:20 +0000

    Hapi Tutorial Series
    Add a route with a path of GET /{any*} which handle any uri does has not been handle before.

--- server.js
+++ server.js
@@ -10,23 +10,36 @@ const init = async () => {
     });
 
     // define the route
-    server.route({
+    server.route([{
         method: 'GET',
         path: '/',
         handler: (request, h) => {
             return '<h1>Hello World!</h1>'
         }
-    });
-
-    // define the route http://localhost.3001/users/
-    server.route({
+    },
+    {
         method: 'GET',
         path: '/users/{user?}',
         handler: (request, h) => {
             return `<h1>${request.query.name}</h1><br/>
                     <h1>${request.params.user}</h1>`;
         }
-    });
+    },
+    {
+        method: 'GET',
+        path: '/home',
+        handler: (request, h) => {
+            return h.redirect('/');
+        }
+    },
+    {
+        method: 'GET',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return "<h1>Oh no! You must be lost!</h1>";
+        }
+    }]);
+
 
     await server.start();
     console.log(`server started on ${server.info.uri}`);
